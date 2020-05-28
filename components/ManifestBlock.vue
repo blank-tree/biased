e2056ec569202950f81d5ce134af0eafc8a6456f
Fernando Obieta <fernando.obieta@blanktree.ch>
2020-05-29 00:18:59 +0200

    correct position and random sorting

---
+++ components/ManifestBlock.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    id="manifest-block"
+    @click="$store.commit('sortManifest')"
+  >
+    <transition-group
+      name="manifest-group" tag="div"
+    >
+      <div
+        v-for="point in posts"
+        :key="point"
+        class="point"
+        v-html="point"
+      />
+    </transition-group>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ManifestBlock",
+    props: {
+      posts: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+  #manifest-block {
+    display: block;
+  }
+
+  .manifest-group-move {
+    transition: transform .75s;
+  }
+
+  .point {
+    margin: 0.5rem;
+    font-weight: 600;
+    letter-spacing: 0.1rem;
+
+    &:after {
+      content: ' '
+    }
+  }
+
+</style>
---
+++ store/index.js
@@ -0,0 +1,36 @@
+export const state = () => ({
+  posts: [
+    'Das Artefakt zeigt seine eigene Gemachtheit und wirkt subjektiv.',
+    'Sich selbst und den Kontext hinterfragen, anstelle einer neuen Lösung.',
+    'Die Ästhetik richtet sich an der Geschichte und/oder dem Kontext aus, nicht den eigenen Präferenzen.',
+    'Missverständnisse und Irritationen sind erwünscht, begründen sich aber auf dem zu reflektierenden Forschungsgegenstand und nicht der Schaulust.',
+    'Die Neusetzung muss in sich funktionieren und darf nicht fingieren.',
+    'Das Artefakt und die/der Erschaffer*in sind nicht wissend und dürfen auch keinen anderen Anschein erwecken.',
+    'Die Einflüsse und Gedanken werden den Betrachter*innen zugänglich gemacht und nicht verschleiert.',
+    'Das Artefakt und die/der Erschaffer*in sind offen für andere Deutungen und Schlüsse.',
+    'Es ist aktuell; im hier und jetzt; zeitlich begrenzt.',
+    'Es ist präzise und demütig.'
+  ]
+});
+
+export const mutations = {
+  sortManifest(state) {
+    state.posts.sort(function (a, b) {
+      return Math.random() > 0.5;
+    });
+    /*
+    let randomised = state.posts;
+    let m = randomised.length, t, i;
+    // While there remain elements to shuffle
+    while (m) {
+      // Pick a remaining element
+      i = Math.floor(Math.random() * m--);
+      // And swap it with the current element.
+      t = randomised[m];
+      randomised[m] = randomised[i];
+      randomised[i] = t;
+    }
+    state.posts = randomised;
+     */
+  },
+};

3ce48df3223d0c846dc92f660be6f71a5cf100e1
Fernando Obieta <fernando.obieta@blanktree.ch>
2020-05-28 22:30:12 +0200

    added manifest

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,18 @@ export default {
   },
   data() {
     return {
-
+      manifest: [
+        'Das Artefakt zeigt seine eigene Gemachtheit und wirkt subjektiv.',
+        'Sich selbst und den Kontext hinterfragen, anstelle einer neuen Lösung.',
+        'Die Ästhetik richtet sich an der Geschichte und/oder dem Kontext aus, nicht den eigenen Präferenzen.',
+        'Missverständnisse und Irritationen sind erwünscht, begründen sich aber auf dem zu reflektierenden Forschungsgegenstand und nicht der Schaulust.',
+        'Die Neusetzung muss in sich funktionieren und darf nicht fingieren.',
+        'Das Artefakt und die/der Erschaffer*in sind nicht wissend und dürfen nicht diesen Anschein erwecken.',
+        'Die Einflüsse und Gedanken werden den Betrachter*innen zugänglich gemacht und nicht verschleiert.',
+        'Das Artefakt und die/der Erschaffer*in sind offen für andere Deutungen und Schlüsse.',
+        'Es ist aktuell; im hier und jetzt; zeitlich begrenzt.',
+        'Es ist präzise und demütig.'
+      ]
     }
   }
 }

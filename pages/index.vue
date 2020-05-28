2d29ab8534e5d5d33e0e996af00888fb3cf5bd64
Fernando Obieta <fernando.obieta@blanktree.ch>
2020-05-28 22:33:02 +0200

    added fisher-yates shuffle algorithm for sorting

--- pages/index.vue
+++ pages/index.vue
@@ -11,26 +11,42 @@
 <script>
 
 
-export default {
-  name: 'biased',
-  components: {
-
-  },
-  data() {
-    return {
-      manifest: [
-        'Das Artefakt zeigt seine eigene Gemachtheit und wirkt subjektiv.',
-        'Sich selbst und den Kontext hinterfragen, anstelle einer neuen Lösung.',
-        'Die Ästhetik richtet sich an der Geschichte und/oder dem Kontext aus, nicht den eigenen Präferenzen.',
-        'Missverständnisse und Irritationen sind erwünscht, begründen sich aber auf dem zu reflektierenden Forschungsgegenstand und nicht der Schaulust.',
-        'Die Neusetzung muss in sich funktionieren und darf nicht fingieren.',
-        'Das Artefakt und die/der Erschaffer*in sind nicht wissend und dürfen nicht diesen Anschein erwecken.',
-        'Die Einflüsse und Gedanken werden den Betrachter*innen zugänglich gemacht und nicht verschleiert.',
-        'Das Artefakt und die/der Erschaffer*in sind offen für andere Deutungen und Schlüsse.',
-        'Es ist aktuell; im hier und jetzt; zeitlich begrenzt.',
-        'Es ist präzise und demütig.'
-      ]
+  export default {
+    name: 'biased',
+    components: {},
+    data() {
+      return {
+        manifest: [
+          'Das Artefakt zeigt seine eigene Gemachtheit und wirkt subjektiv.',
+          'Sich selbst und den Kontext hinterfragen, anstelle einer neuen Lösung.',
+          'Die Ästhetik richtet sich an der Geschichte und/oder dem Kontext aus, nicht den eigenen Präferenzen.',
+          'Missverständnisse und Irritationen sind erwünscht, begründen sich aber auf dem zu reflektierenden Forschungsgegenstand und nicht der Schaulust.',
+          'Die Neusetzung muss in sich funktionieren und darf nicht fingieren.',
+          'Das Artefakt und die/der Erschaffer*in sind nicht wissend und dürfen nicht diesen Anschein erwecken.',
+          'Die Einflüsse und Gedanken werden den Betrachter*innen zugänglich gemacht und nicht verschleiert.',
+          'Das Artefakt und die/der Erschaffer*in sind offen für andere Deutungen und Schlüsse.',
+          'Es ist aktuell; im hier und jetzt; zeitlich begrenzt.',
+          'Es ist präzise und demütig.'
+        ]
+      }
+    },
+    methods: {
+      sort: function () {
+        // Fisher–Yates shuffle algorithm
+        // https://bost.ocks.org/mike/shuffle/
+        let randomised = this.manifest;
+        let m = randomised.length, t, i;
+        // While there remain elements to shuffle
+        while (m) {
+          // Pick a remaining element
+          i = Math.floor(Math.random() * m--);
+          // And swap it with the current element.
+          t = randomised[m];
+          randomised[m] = randomised[i];
+          randomised[i] = t;
+        }
+        this.manifest = randomised;
+      }
     }
   }
-}
 </script>

b7ad7c463e5dbf462624d13de477f3486558e742
Fernando Obieta <fernando.obieta@blanktree.ch>
2020-05-28 22:28:49 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row>
+    <v-col>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+
+export default {
+  name: 'biased',
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
